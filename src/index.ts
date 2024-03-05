e0c4a0c056bd6a67503992689618a499d8a7c2e9
Quentin <qgransart@goweb.fr>
2024-03-05 16:47:57 +0100

    reponse_7

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 
 interface Person {
   age: number;
-  taille: number;
+  [key: string]: number;
 }
 
 interface Statistics {
@@ -10,22 +10,47 @@ interface Statistics {
   meanTaille: number;
 }
 
-function getStatistics(): Statistics {
-  const persons: Person[] = JSON.parse(
-    readFileSync('./persons.json').toString()
-  );
+function getStatistics(): Statistics | null {
+  try {
+    const persons: Person[] = JSON.parse(
+      readFileSync('./persons.json').toString()
+    );
 
-  const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
-  const totalTaille = persons.reduce((sum, person) => sum + person.taille, 0);
+    if (
+      !persons.length ||
+      !Object.hasOwnProperty.call(persons[0], 'age') ||
+      !Object.hasOwnProperty.call(persons[0], 'taille')
+    ) {
+      throw new Error(
+        'Les données du fichier persons.json ne sont pas correctement formées.'
+      );
+    }
 
-  const meanAge = totalAge / persons.length;
-  const meanTaille = totalTaille / persons.length;
+    const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
+    const totalTaille = persons.reduce((sum, person) => sum + person.taille, 0);
 
-  return { meanAge, meanTaille };
+    const meanAge = totalAge / persons.length;
+    const meanTaille = totalTaille / persons.length;
+
+    return { meanAge, meanTaille };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("Une erreur inattendue s'est produite.");
+    }
+    return null;
+  }
 }
 
 function displayResult() {
-  console.log(getStatistics());
+  const statistics = getStatistics();
+
+  if (statistics !== null) {
+    console.log(statistics);
+  } else {
+    console.error('Impossible de calculer les statistiques.');
+  }
 }
 
 displayResult();
