c8e471a5c6b273ce6a11c9c1b90d9d1ed9fcf62e
Quentin <qgransart@goweb.fr>
2024-03-05 16:43:43 +0100

    reponse_1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
-function getStatistics() {
-  const persons = JSON.parse(readFileSync("./persons.json").toString());
+interface Person {
+  age: number;
+  height: number;
+}
+
+function getStatistics(): number {
+  const persons: Person[] = JSON.parse(
+    readFileSync('./persons.json').toString()
+  );
+
+  const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
+  const averageAge = totalAge / persons.length;
+
+  return averageAge;
 }
 
 function displayResult() {
   console.log(getStatistics());
 }
+
 displayResult();
